6bc8fd6084dc4e6f873682482d58aa8eb78c0479
Mat-Frayne <mat.frayne@gmail.com>
2019-04-07 17:25:07 +0930

    .

--- static/js/template.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-    $.get("/static/CanvasWorld/" + t + "/scripts/index.js", function (data) {
-        $("#code pre code").text(data)
-        document.querySelectorAll('pre code').forEach((block) => {
-            hljs.highlightBlock(block);
-        });
-
-    })
-});
