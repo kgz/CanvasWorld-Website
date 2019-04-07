bb6792bf45632fdfd6147bb637c4225a62ba8635
Mat-Frayne <mat.frayne@gmail.com>
2019-04-07 16:47:38 +0930

    first commit

---
+++ app.py
@@ -0,0 +1,56 @@
+"""."""
+from flask import Flask, flash, json, jsonify, render_template, request, send_file
+from PyLog import Log, Logger
+import dataset
+import os
+from werkzeug.utils import secure_filename
+from datetime import datetime
+
+ 
+app = Flask(__name__, template_folder="static/CanvasWorld")
+app.debug = True
+files =[[secure_filename(x.name),x.name, datetime.fromtimestamp(x.stat().st_ctime).strftime('%Y-%m-%d')] for x in os.scandir("static/CanvasWorld") if x.is_dir() and not x.name.startswith(".")]
+files.sort(key=lambda tup: tup[2])
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
+
+    
+def pathn(name):
+   """."""
+   try:
+      return render_template(f"{name[1]}/index.html", args={"name" : name, "scripts":files})
+   except:
+      return 404
+
+for filename in files:
+    app.add_url_rule("/" + filename[0], defaults={"name": filename}, view_func=pathn)
+
+
+@app.route("/sample/<path>")
+def getsample(path):
+   """."""
+   p = f"static/CanvasWorld/{path}/sample.gif"
+   if not os.path.isfile(p):
+      p = "static/images/404.gif"
+   return send_file(p, mimetype='image/gif')
+
+@app.route('/')
+def method_name():
+   return render_template(".index.html", files = files)
+
+
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80, debug=True)
+
+
