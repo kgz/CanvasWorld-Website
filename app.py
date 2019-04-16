3e2bfb0ebab5e9fe13a6408b230c126edc971f68
Mat-Frayne <mat.frayne@gmail.com>
2019-04-16 16:55:42 +0930

    .

--- app.py
+++ app.py
@@ -6,24 +6,29 @@ import os
 from werkzeug.utils import secure_filename
 from datetime import datetime
 from werkzeug.security import check_password_hash
+import hashlib 
+import random
+import string
+import smtplib
+from email.message import EmailMessage
 
- 
 app = Flask(__name__, template_folder="static/CanvasWorld")
 app.debug = True
 files =[[secure_filename(x.name),x.name, datetime.fromtimestamp(x.stat().st_ctime).strftime('%Y-%m-%d')] for x in os.scandir("static/CanvasWorld") if x.is_dir() and not x.name.startswith(".")]
 files.sort(key=lambda tup: tup[2])
-key = 'pbkdf2:sha256:50000$kcKh6aKg$2df02e4cc530afad0e67f3b9f3b27e0aba561cfdaf91866814b3aa56acd86fee'
-k = check_password_hash(key, "LNca8HA9DYrBtpe4")
-Log(k)
+# key = 'pbkdf2:sha256:50000$kcKh6aKg$2df02e4cc530afad0e67f3b9f3b27e0aba561cfdaf91866814b3aa56acd86fee'
+# k = check_password_hash(key, "LNca8HA9DYrBtpe4")
+# Log(k)
 
+db = dataset.connect("sqlite:///logs.db?check_same_thread=false")
 
-logs = dataset.connect("sqlite:///logs.db?check_same_thread=false")["logs"]
+logs = db["logs"]
 
+keys = ''.join([random.choice(string.hexdigits) for x in range(random.randrange(20, 30))])
+with open(".h", "w+") as f:
+   f.write(keys)
+keys = hashlib.md5(keys.encode()).hexdigest()
 
-# @app.before_request
-# def before():
-#    """."""
-#    Log(request)
 
 
 def log():
@@ -35,10 +40,26 @@ def log():
          ip = request.remote_addr,
          env_ip = request.environ['REMOTE_ADDR']
    ))
+@app.route("/admin/<key>")
+def add(key=None):
+   """."""
+   log()
+
+   if key:
+      if hashlib.md5(key.encode()).hexdigest() == keys:
+         return render_template(".admin.html", logs=[x for x in logs.all()])
+      
+   return "", 502
 
 
+@app.route("/static/CanvasWorld/.index.html")
+@app.route("/static/CanvasWorld/.template.html")
+@app.route("/static/CanvasWorld/<a>/index.html")
+@app.route("/static/CanvasWorld/.admin.html")
+def block(a=""):
+   return "", 404
 
-
+   
 @app.after_request
 def add_header(r):
     """
